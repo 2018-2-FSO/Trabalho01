6c15d9c0072aa8bdfe035bf66fd2fc14f4434f03
gustavoduartemoreira <gustavoduartemoreira@gmail.com>
2018-11-08 01:49:58 -0200

    problema2 vf

--- problema2.c
+++ problema2.c
@@ -1,16 +1,16 @@
-#include <stdio.h>//ok
-#include <stdlib.h>//ok
-#include <pthread.h>//ok
-#include <semaphore.h>//ok
-#include <unistd.h>//ok
-#include <time.h>//ok
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
+#include <time.h>
 
-int randomizaNumEstudantes();//ok
-void *estudante (void *param);//ok
-void *monitor(void *param);//ok
-void criaThreadsEstudantes(pthread_t *estudantes, pthread_attr_t *attrEstudantes, int *idEstudantes, int quantidade);//ok
-int numero_estudantes_atendidos = 0;//ok
-int cad_ocup = 0;//ok
+int randomizaNumEstudantes();
+void *estudante (void *param);
+void *monitor(void *param);
+void criaThreadsEstudantes(pthread_t *estudantes, pthread_attr_t *attrEstudantes, int *idEstudantes, int quantidade);
+int numero_estudantes_atendidos = 0;
+int cad_ocup = 0;
 int num_cadeiras;
 int cadeira_monitor;
 int quantidade=0;
@@ -18,7 +18,7 @@ int quantidade=0;
 pthread_t monitor_tid; 
 pthread_attr_t attrMonitor;
 
-sem_t cadeira_queue, soneca_monitor, ajudando_estudante;
+sem_t cadeiraAtendimento, soneca_monitor, ajudando_estudante;
 pthread_mutex_t cad_mutex, estudante_mutex; 
 
 void acorda_monitor(void){
@@ -34,7 +34,7 @@ void ajuda_estudante(void){
 int main (){
 
     srand(time(NULL));
-    sem_init(&cadeira_queue, 0, 1);
+    sem_init(&cadeiraAtendimento, 0, 1);
 	sem_init(&soneca_monitor, 0, 1);
 	sem_init(&ajudando_estudante, 0, 1);
 	pthread_mutex_init(&cad_mutex, NULL);
@@ -43,45 +43,25 @@ int main (){
 	sem_wait(&soneca_monitor); //faz o monitor tirar uma soneca
 	sem_wait(&ajudando_estudante); //faz o estudante esperar
 
-    int *idEstudantes;
-    int i;
+    int i, *idEstudantes;
     
     pthread_attr_init(&attrMonitor);
 	pthread_create(&monitor_tid,&attrMonitor, monitor, NULL);
-    quantidade = randomizaNumEstudantes();
-    printf("quantidade %d\n", quantidade);
-    num_cadeiras = quantidade/2;
+    quantidade = randomizaNumEstudantes(); //randomiza a quantidade de estudantes
+    printf("Quantidade de estudantes %d\n", quantidade);
+    num_cadeiras = quantidade/2;  //numero de cadeiras dividido por 2
     printf("Numero de cadeiras é %d\n", num_cadeiras);
 
     pthread_t *estudantes;
     pthread_attr_t *attrEstudantes;
     
-
-    //pthread_t monitor_tid;
-    //pthread_attr_t attrMonitor;
-    //Cria thread monitor
-	
-
-    
     estudantes = (pthread_t *) malloc(sizeof(pthread_t)*quantidade); // cria um ponteiro com n estudantes
     attrEstudantes = (pthread_attr_t *) malloc (sizeof (pthread_attr_t)*quantidade); 
     idEstudantes = (int*) malloc(sizeof(int)*quantidade);
-
-     for (int i = 0 ; i < quantidade; i++){
-        idEstudantes[i] = i;
-        pthread_attr_init(&attrEstudantes[i]);
-        pthread_create(&estudantes[i], &attrEstudantes[i], estudante, &idEstudantes[i]);    
-    }
-
-	for(i=0; i < quantidade; i++){
-		pthread_join(estudantes[i], NULL);
-	}
+    criaThreadsEstudantes(estudantes, attrEstudantes, idEstudantes, quantidade); //cria as threads de estudante
     pthread_join(monitor_tid, NULL);
 
-    //sem_destroy(&cadeira_monitor);
-	sem_destroy(&soneca_monitor);
-	pthread_mutex_destroy(&cad_mutex);
-	pthread_mutex_destroy(&estudante_mutex);
+
 	free(estudantes);
 	free(attrEstudantes);
 	free(idEstudantes);
@@ -91,17 +71,21 @@ int main (){
 int randomizaNumEstudantes()
 {
     int quantidade;
-    quantidade = (rand()%(37)) + 3;
+    quantidade = (rand()%(38)) + 3;
     return quantidade;
 }
 
-//Não consegui chamar no main(), copiei o for direto!
 void criaThreadsEstudantes(pthread_t *estudantes, pthread_attr_t *attrEstudantes, int *idEstudantes, int quantidade){
 
     for (int i = 0 ; i < quantidade; i++){
         idEstudantes[i] = i;
         pthread_attr_init(&attrEstudantes[i]);
-        pthread_create(&estudantes[i], &attrEstudantes[i], estudante, &idEstudantes[i]);    
+        pthread_create(&estudantes[i], &attrEstudantes[i], estudante, &idEstudantes[i]); 
+        printf("Estudante %d criado\n", i);   
+    }
+
+    for(int i = 0; i <quantidade; i++){ //espera a execução das threads
+        pthread_join(estudantes[i], NULL);
     }
 
 }
@@ -116,18 +100,19 @@ int pede_ajuda(int idEstudantes){
     if(cad_ocup < num_cadeiras){
         pthread_mutex_lock(&cad_mutex);
         cad_ocup ++;
+        printf("numero de cadeiras ocupadas %d\n", cad_ocup);
         pthread_mutex_unlock(&cad_mutex);
-        sem_wait(&cadeira_queue);
+        sem_wait(&cadeiraAtendimento);
 		cadeira_monitor = idEstudantes;
 		printf("O estudante %d vai ser Atendido.\n", idEstudantes);
-        //sem_post(&soneca_monitor);
+        //sem_post(&soneca_monitor); //implemtado na função acorda_monitor();
         //sem_wait(&ajudando_estudante);
 		acorda_monitor();
 		ajuda_estudante();
 		cadeira_monitor = -1;
-        printf("Acorda monitor!\n");
-		printf("O estudante %d foi Atendido.\n", idEstudantes);
-		sem_post(&cadeira_queue);
+        //printf("Acorda monitor!\n");
+		//printf("O estudante %d foi Atendido.\n", idEstudantes);
+		sem_post(&cadeiraAtendimento);
 		pthread_mutex_lock(&cad_mutex);
 		cad_ocup--;
 		pthread_mutex_unlock(&cad_mutex);
@@ -168,15 +153,18 @@ void *estudante (void *param){
 void *monitor(void *param){
 
 	while(1){
-		printf("O monitor vai tirar uma soneca!.\n");
+		if(cad_ocup == 0){ // se nao houver ngm para ser atendido ele dorme
+        printf("O monitor vai tirar uma soneca!.\n");
 		sem_wait(&soneca_monitor);
-        int ajuda_temp = rand() % 100000 + 100000;
+        }
+        else{
+        int ajuda_temp = rand() % 100000 + 100000; // tempo aleatorio de atendimento
 		printf("O monitor vair atender o estudante num %d.\n",cadeira_monitor);
 
 		usleep(ajuda_temp);
 		printf("O estudante %d foi atendido.\n",cadeira_monitor);
 		sem_post(&ajudando_estudante);
+        }
 	}
-	printf("Acabou!\n");
 	pthread_exit(0);
 }
